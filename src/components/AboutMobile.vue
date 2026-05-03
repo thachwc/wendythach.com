37a3b5acc38da8d3fe5457cae1cb7b1f0a53284b
Wendy Thach <120411231+thachwc@users.noreply.github.com>
2026-05-03 18:49:40 -0400

    Feature/update (#4)
    * Create CNAME
    
    * Create deploy.yml
    
    * Changed default branch on deploy
    
    * Fixed bug
    
    * Update Mobile View

---
+++ src/components/AboutMobile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="container">
+    <div id="about">
+      <p><span id="intro">Hello there!</span><br>
+        I'm Wendy Thach, a 26 year old Vietnamese American and Computer Science graduate who specializes on UI/UX design
+        and CAD.<br>I'm detail-oriented and passionate about blending creativity with technology, using tools like Figma
+        to
+        design intuitive and engaging user experiences.
+        <br>Outside of work, I enjoy baking, cake decorating, and gaming.
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped>
+#container {
+  width: 336px;
+  height: 442px;
+}
+
+#about {
+  display: flex;
+  align-items: center;
+  width: 336px;
+  height: 442px;
+  text-align: center;
+}
+
+#intro {
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 900;
+}
+
+p {
+  text-align: center;
+  color: #D9A5A9;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 900;
+  padding: 5px;
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,34 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const isMobile = ref(window.innerWidth <= 769)
+
+const handleResize = () => {
+  isMobile.value = window.innerWidth <= 769
+}
+
+onMounted(() => window.addEventListener('resize', handleResize))
+onUnmounted(() => window.removeEventListener('resize', handleResize))
+</script>
+
 <template>
-    <img src="../assets/BottomCurtains.svg" alt="Bottom Curtains">
+  <img v-if="isMobile" src="../assets/BottomMobile.svg" alt="Bottom Curtains">
+  <img v-else src="../assets/BottomCurtains.svg" alt="Bottom Curtains">
 </template>
 <style scoped>
 img {
+  position: fixed;
+  top: 797px;
+  width: 1270px;
+  border-radius: 0px 0px 20px 20px;
+}
+
+@media (max-width: 769px) {
+  img {
     position: fixed;
-    top: 797px;
-    width: 1270px;
+    top: 646px;
+    width: 336px;
     border-radius: 0px 0px 20px 20px;
+  }
 }
 </style>
